6c8610d22b8736ba01a71ad36a3ebf72bdfab3d1
Ben <Benl000100@gmail.com>
2022-02-17 20:08:47 +0200

    commit 9

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -5,7 +5,7 @@
 function renderGallery() {
   var strHTML = '';
   strHTML += gImgs.map((img) => {
-    return `<img href="#" src="./img/square/${img.id}.jpg" onclick="onImgSelect(${img.id})" alt="">`;
+    return `<img href="#main-header" src="./img/square/${img.id}.jpg" onclick="onImgSelect(${img.id})" alt="">`;
   }).join('');
   var elGallery = document.querySelector('.main-gallery');
   elGallery.innerHTML = strHTML;
